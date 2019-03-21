cae3e96275b9163080e06e6a11192d7de179f22d
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2019-03-20 20:28:58 -0500

    updated to fix typo

--- bookmarklet.js
+++ bookmarklet.js
@@ -11,7 +11,7 @@ javascript: (function () {
   let color = prompt('Background Color? Hex or name will do', 'blue');
   document.body.style.backgroundColor = color;
   let divElements = document.querySelectorAll('div');
-  for (elements of arrElements) {
+  for (elements of divElements) {
 	   elements.style.backgroundColor = color;
   }
 })();
